6c3e3a93a62f06a719412e7b3b757fe799e13717
Yassir achamrar <yassir.achamrar@gmail.com>
2020-04-20 21:50:35 +0100

    Update

---
+++ PFE/PFE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PFE.Startup))]
+namespace PFE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
